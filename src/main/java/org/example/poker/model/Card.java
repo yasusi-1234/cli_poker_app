9415e2f42d34526dd6dbf287526f0f9c05c0fd27
yasu <yasusi0117@live.jp>
2022-07-04 17:44:49 +0900

    Cardクラスのフィールド・コンストラクタ・メソッド作成

--- src/main/java/org/example/poker/model/Card.java
+++ src/main/java/org/example/poker/model/Card.java
@@ -1,4 +1,40 @@
 package org.example.poker.model;
 
+import java.util.Objects;
+
+/**
+ * @author yasu
+ * @version 1.0
+ * ポーカーのカードを現すクラス
+ */
 public class Card {
+
+    private final int cardNumber;
+    private final CardType cardType;
+
+    public Card(int cardNumber, CardType cardType) {
+        this.cardNumber = cardNumber;
+        this.cardType = cardType;
+    }
+
+    public int getCardNumber() {
+        return cardNumber;
+    }
+
+    public CardType getCardType() {
+        return cardType;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Card)) return false;
+        Card card = (Card) o;
+        return cardNumber == card.cardNumber && cardType == card.cardType;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cardNumber, cardType);
+    }
 }
