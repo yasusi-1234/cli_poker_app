32689de59bcd65ecb59fc8e6336c48bae68a10d3
yasu <yasusi0117@live.jp>
2022-07-04 18:06:22 +0900

    docの追加
    コンストラクタで異常値が挿入されないようロジックの追加

--- src/main/java/org/example/poker/model/Card.java
+++ src/main/java/org/example/poker/model/Card.java
@@ -9,10 +9,26 @@ import java.util.Objects;
  */
 public class Card {
 
+    /**
+     * カードの番号 1~13
+     */
     private final int cardNumber;
+    /**
+     * カードのマーク スペード・ハート・クラブ・ダイヤの4種類
+     */
     private final CardType cardType;
 
+    /**
+     * コンストラクタ
+     * @param cardNumber カードの番号
+     * @param cardType カードのマーク
+     */
     public Card(int cardNumber, CardType cardType) {
+        if(cardNumber < 1 || cardNumber > 13){
+            throw new IllegalArgumentException(
+                    "カードの番号が想定の範囲外です。入力された数値 -> " + cardNumber +
+                    " 1~13の値を選択してください。");
+        }
         this.cardNumber = cardNumber;
         this.cardType = cardType;
     }
