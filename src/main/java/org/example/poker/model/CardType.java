f14235acbb5f1c7c67543dd1b26e59a79cfc9e02
yasu <yasusi0117@live.jp>
2022-07-04 17:53:00 +0900

    カードタイプクラスのフィールド・コンストラクタ・メソッドの作成

--- src/main/java/org/example/poker/model/CardType.java
+++ src/main/java/org/example/poker/model/CardType.java
@@ -1,4 +1,15 @@
 package org.example.poker.model;
 
 public enum CardType {
+    SPADE("♠"), HEART("♥"), CLUB("♣"), DIAMOND("♦");
+
+    CardType(String cardMark) {
+        this.cardMark = cardMark;
+    }
+
+    private final String cardMark;
+
+    public String getCardMark() {
+        return cardMark;
+    }
 }
