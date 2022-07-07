df4eed384c6961b71e6799bde247922fa3528ac6
yasu <yasusi0117@live.jp>
2022-07-07 17:45:51 +0900

    使われていないimport文の削除

--- src/test/java/org/example/poker/service/PokerHandTest.java
+++ src/test/java/org/example/poker/service/PokerHandTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
