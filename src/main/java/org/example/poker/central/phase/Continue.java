6ff2ff6bc9ce26f7570dd00fcf7d8b18cd567065
yasu <yasusi0117@live.jp>
2022-07-12 21:18:23 +0900

    gamePhaseを継承したContinueクラスの作成

---
+++ src/main/java/org/example/poker/central/phase/Continue.java
@@ -0,0 +1,44 @@
+package org.example.poker.central.phase;
+
+import org.example.poker.central.PokerGameSystem;
+import org.example.poker.service.CardDealer;
+import org.example.poker.service.PlayerResponse;
+import org.example.poker.service.PokerPlayer;
+import org.example.poker.view.PokerPrinter;
+
+public class Continue implements GamePhase{
+
+    private static final GamePhase singleton = new Continue();
+
+    private Continue(){}
+
+    @Override
+    public void advanceGame(PokerGameSystem pokerGameSystem) {
+        PokerPlayer pokerPlayer = pokerGameSystem.getPokerPlayer();
+        PokerPrinter pokerPrinter = pokerGameSystem.getPokerPrinter();
+        CardDealer cardDealer = pokerGameSystem.getCardDealer();
+        // ゲームを続けるか等をプレイヤーに尋ねる
+        PlayerResponse response = pokerPlayer.gameContinue();
+
+        switch (response){
+            case CONTINUE:
+                cardDealer.cardsShuffle();
+                // ゲーム初回フェーズへ移行
+                pokerGameSystem.setGamePhase(FirstDraw.getInstance());
+                break;
+            case SHOW_STATUS:
+                pokerPrinter.printUserScore(pokerPlayer.getPokerResultMap(),
+                                pokerPlayer.getScore(),
+                                pokerPlayer.getGameCount());
+                break;
+            case GAME_END:
+                pokerGameSystem.setGamePhase(GameEnd.getInstance());
+                break;
+        }
+
+    }
+
+    public static GamePhase getInstance() {
+        return singleton;
+    }
+}
