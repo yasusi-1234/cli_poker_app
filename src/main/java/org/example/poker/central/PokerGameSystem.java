e291b6a5db283b9b478b7ec80b05f8be0038c860
yasu <yasusi0117@live.jp>
2022-07-12 21:27:25 +0900

    fieldにdoc追記
    gamePhaseフィールドの追加・gamePhaseのセッターメソッド追加
    
    pokerPlayer, pokerPrinter, pokerHandAnalyst, cardDealer のゲッターメソッド追加
    
    コンストラクタの修正
    
    gamePlayメソッドのロジック変更

--- src/main/java/org/example/poker/central/PokerGameSystem.java
+++ src/main/java/org/example/poker/central/PokerGameSystem.java
@@ -1,88 +1,72 @@
 package org.example.poker.central;
 
-import org.example.poker.model.Card;
+import org.example.poker.central.phase.FirstDraw;
+import org.example.poker.central.phase.GameEnd;
+import org.example.poker.central.phase.GamePhase;
 import org.example.poker.service.*;
 import org.example.poker.view.PokerPrinter;
 
-import java.util.List;
-import java.util.Set;
 
+/**
+ * ポーカーゲームの進行を管理するクラス
+ *
+ * @author yasu
+ * @version 1.0
+ */
 public class PokerGameSystem {
 
+    /** {@link PokerPlayer} ポーカーのプレイヤー */
     private final PokerPlayer pokerPlayer;
 
+    /** {@link PokerPrinter} ポーカーの画面表示 */
     private final PokerPrinter pokerPrinter;
 
+    /** {@link PokerHandAnalyst} ポーカーの手役解析をするオブジェクト */
     private final PokerHandAnalyst pokerHandAnalyst;
 
+    /** {@link CardDealer} ポーカーで使うカードを管理するオブジェクト */
     private final CardDealer cardDealer;
 
+    /** {@link GamePhase} ゲームフェーズを現すオブジェクト*/
+    private GamePhase gamePhase;
+
+    /** コンストラクタ */
     public PokerGameSystem() {
         this.pokerPlayer = new PokerPlayer();
         this.pokerPrinter = new PokerPrinter();
         this.pokerHandAnalyst = new PokerHandAnalyst();
         this.cardDealer = new CardDealer();
+        // ゲームフェーズのセット
+        this.setGamePhase(FirstDraw.getInstance());
     }
 
+    /** ゲームプレイ */
     public void gamePlay() {
 
-        boolean gameContinue = true;
-
-        while(gameContinue){
-
-            // 初回のカードを格納
-            List<Card> playerCards = cardDealer.firstDistributeCards();
-            // プレイヤーにカードを格納
-            pokerPlayer.setCards(playerCards);
-
-            Set<Integer> dropCardIndexes;
-            while(true){
-                // プレイヤーの配られたカード情報を表示
-                pokerPrinter.printUserCards(pokerPlayer.getCards());
-                // プレイヤーに捨てるカードを選択してもらう
-                dropCardIndexes = pokerPlayer.cardSelection();
-                // プレイヤーのカードの取捨状況の表示
-                pokerPrinter.printUserCardsSelection(pokerPlayer.getCards(), dropCardIndexes);
-                // プレイヤーの取捨状況の確認
-                boolean matchChoice = pokerPlayer.cardChangeCheck();
-
-                if (matchChoice){
-                    break;
-                }
-
-            }
-
-            // 捨てたカードに新たなカードを挿入した結果
-            List<Card> newPlayerCards = cardDealer.distributeCards(pokerPlayer.getCards(), dropCardIndexes);
-            // プレイヤーのカードにセット
-            pokerPlayer.setCards(newPlayerCards);
-            // プレイヤーのカードの手役抽出
-            PokerHand hand = pokerHandAnalyst.analyzePokerHand(newPlayerCards);
-            // 上がり役の格納と、スコアをプラス
-            pokerPlayer.addPokerResultMap(hand);
-            pokerPlayer.addScore(hand.getScore());
-            // 結果表示
-            pokerPrinter.printUserCardsResult(newPlayerCards, "やったね！");
-
-            while(true){
-                // ゲームを続けるか等をプレイヤーに尋ねる
-                PlayerResponse response = pokerPlayer.gameContinue();
-
-                if(response == PlayerResponse.CONTINUE){
-                    cardDealer.cardsShuffle();
-                    break;
-                }else if (response == PlayerResponse.GAME_END){
-                    gameContinue = false;
-                    break;
-                } else if (response == PlayerResponse.SHOW_STATUS) {
-                    pokerPrinter.printUserScore(
-                            pokerPlayer.getPokerResultMap(), pokerPlayer.getScore());
-                }
-
-            }
+        while(gamePhase.getClass() != GameEnd.class){
+            gamePhase.advanceGame(this);
         }
 
-        System.out.println("ゲームを終了します!!");
+        System.out.println("プレイして頂きありがとうございます！！");
+    }
 
+    public PokerPrinter getPokerPrinter() {
+        return this.pokerPrinter;
+    }
+
+    public PokerPlayer getPokerPlayer() {
+        return pokerPlayer;
+    }
+
+    public PokerHandAnalyst getPokerHandAnalyst() {
+        return pokerHandAnalyst;
+    }
+
+    public CardDealer getCardDealer() {
+        return cardDealer;
+    }
+
+    public void setGamePhase(GamePhase gamePhase) {
+        this.gamePhase = gamePhase;
     }
 }
